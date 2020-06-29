c63b0f648d542dd99366c31e208209abf70d466d
fluRinji <libertisiti9123@gmail.com>
2020-06-29 20:54:44 +0300

    Изменение структуры данных в бд

--- models/Room.js
+++ models/Room.js
@@ -2,8 +2,16 @@ const {Schema, model, Types} = require('mongoose');
 
 const schema = new Schema({
     roomId: {type: String, required: true, unique: true},
-    users: {type: Array, require: true, unique: true},
-    messages: {type: Array}
+    users: [{
+        Name: String,
+        socketId: String
+    }]
+    // messages: {type: Array}
+
 });
 
 module.exports = model('Room', schema);
+
+
+// roomId: {type: String, required: true, unique: true},
+// users: {type: Array, require: true, unique: true},
