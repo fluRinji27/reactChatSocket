f07b661524de95d10ae7a53cd0fb191cd96ffd9f
fluRinji <libertisiti9123@gmail.com>
2020-06-29 15:03:21 +0300

    Модификация серверной чатси: Добавление подключения к базе данных

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const socket = require('socket.io');
 const config = require('config');
 
@@ -8,26 +9,35 @@ const PORT = config.get('PORT');
 //Инициализируем константу нашего сервера
 const app = express();
 
+app.use(express.json({extended: true}));
+app.use('/api/room', require('./routes/room.routes'));
+
 //Инициализируем http сервер для сокетов
 const server = require('http').Server(app);
 
 //Инициализируем сокеты
 const io = socket(server);
 
-//Создаем начальнйы роут
-app.get('/', (req, res) => {
-    res.send()
-});
+const start = async () => {
+    try {
+        //Подключаемся к бае данных
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
 
-//Прослушка на порт и отловка ошибок
-server.listen(PORT, err => {
-    if (err) {
-        throw Error(err)
+        //Прослушиваем каждое подключение
+        await io.on('connection', socket => {
+            console.log('Пользователь подключился:', socket.id)
+        });
+
+        //Прослушка на порт
+        server.listen(PORT, () => console.log('Server has ben started on port: ', PORT));
+    } catch (e) {
+        process.exit(1)
     }
-    console.log('Server has ben started on port: ', PORT)
-});
 
-//Прослушиваем каждое подключение
-io.on('connection', socket => {
-    console.log('Пользователь подключился:', socket)
-});
+};
+
+start();
