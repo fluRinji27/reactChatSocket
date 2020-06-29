027bbf1c945878cafcfa92542bb94f27f2baed2a
fluRinji <libertisiti9123@gmail.com>
2020-06-29 20:51:19 +0300

    Добавление редьюсера

---
+++ client/src/redux/reducer.js
@@ -0,0 +1,14 @@
+export default (state, action) => {
+    switch (action.type) {
+        case 'JOINED':
+            return {
+                ...state,
+                Joined: true,
+                roomId: action.payload.roomId,
+                userName: action.payload.userName,
+            };
+
+        default:
+            return state;
+    }
+}
