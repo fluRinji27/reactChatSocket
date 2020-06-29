f8f61539b50df75457bb940fd1a85ebb4ebabb59
fluRinji <libertisiti9123@gmail.com>
2020-06-29 20:55:38 +0300

    инициализация редьюсера подключение сокетов при входе в комнату

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 import JoinRoom from "./components/JoinRoom/JoinRoom";
+import reducer from "./redux/reducer";
+import socket from "./hooks/socket.hook";
 
 
 function App() {
+    const [state, dispatch] = useReducer(reducer, {
+        Joined: false,
+        roomId: null,
+        userName: null
+
+    });
+
+    const onLogin = (userData) => {
+        dispatch({
+            type: 'JOINED',
+            payload: userData
+        });
+        socket.emit('ROOM:JOIN', userData)
+    };
+
+    console.log(state);
+
     return (
         <div className="App">
-            <JoinRoom/>
+            {!state.Joined && <JoinRoom onLogin={onLogin}/>}
         </div>
     );
 }
