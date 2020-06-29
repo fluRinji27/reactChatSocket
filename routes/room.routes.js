9d678ed3ba4b7e433513334d0162ada981068c70
fluRinji <libertisiti9123@gmail.com>
2020-06-29 20:52:03 +0300

    Поиск существующей и созднаие новой комнаты

--- routes/room.routes.js
+++ routes/room.routes.js
@@ -1,16 +1,33 @@
-const {Router} = require('express')
-const config = require('config')
-const router = Router()
-
+const {Router} = require('express');
+const config = require('config');
+const router = Router();
+const Room = require('../models/Room');
 
 // api/room/join
 router.post('/join', async (req, res) => {
     try {
-        console.log('body,', req.body);
 
         const {roomId, userName} = req.body;
 
-        res.status()
+        const rooms = await Room.findOne({roomId});
+
+        if (rooms) {
+            return res.status(200).json({message: "Вход в комнату выполнен"})
+        }
+
+        const users = [{
+            Name: userName,
+            socketId: null
+        }];
+
+        console.log(users);
+
+        const room = new Room({roomId, users});
+
+
+        await room.save();
+
+        res.status(201).json({message: "Комната создана"})
     } catch (e) {
 
     }
