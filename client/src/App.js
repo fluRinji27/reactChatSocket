bb72ffd0dd4d8d34d1f99ec3193dfd166250ee45
fluRinji <libertisiti9123@gmail.com>
2020-07-03 12:46:19 +0300

    1) Оповещение сокетов о новых данных

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import React, {useReducer} from 'react';
+import React, {useEffect, useReducer, useState} from 'react';
+
 import JoinRoom from "./components/JoinRoom/JoinRoom";
+import Chat from "./components/Chat/Chat";
 import reducer from "./redux/reducer";
 import socket from "./hooks/socket.hook";
 
@@ -8,23 +10,42 @@ function App() {
     const [state, dispatch] = useReducer(reducer, {
         Joined: false,
         roomId: null,
-        userName: null
+        userName: null,
+        socketId: null,
+        users: [],
+        messages: []
 
     });
-
     const onLogin = (userData) => {
         dispatch({
             type: 'JOINED',
             payload: userData
         });
+
+        // Отправяем по сокета данные о попытке входа в комнате
         socket.emit('ROOM:JOIN', userData)
     };
 
-    console.log(state);
+    useEffect(() => {
+        // Фиксируем вход пользователя в комнату
+        socket.on('ROOM:JOINED', users => {
+            dispatch({
+                    type: 'SET_USERS',
+                    payload: users
+                }
+            );
+
+            socket.emit('ROOM:JOINED', users)
+            console.log('ROOM:JOINED', users)
+        })
+    }, []);
+
+    console.log(state)
 
     return (
         <div className="App">
-            {!state.Joined && <JoinRoom onLogin={onLogin}/>}
+            {!state.Joined ? <JoinRoom onLogin={onLogin} socket={socket}/> : <Chat users={state}/>}
+
         </div>
     );
 }
