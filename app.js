8bc8e7efe00f2258a3ca3da20504dc68211600f4
fluRinji <libertisiti9123@gmail.com>
2020-07-03 12:42:43 +0300

    1) Логика добавления пользователя вынесена в routes.route.js 2) Добавлена лоига создания отдельный зоны видимости оскетов т.е ограничение на оповещение. Оповещения получают толлько пользователи в пределе комнаты 3) Добавлена логика оповещения пользователя, о другом вошедшем пользователе 4) Добавлена логика дисконекта пользователя

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const socket = require('socket.io');
 const config = require('config');
 const Room = require('./models/Room');
 
-
 //Константа которая получает порт сервера из config
 const PORT = config.get('PORT');
 
@@ -20,51 +19,58 @@ const server = require('http').Server(app);
 //Инициализируем сокеты
 const io = socket(server);
 
+
 const start = async () => {
     try {
         //Подключаемся к бае данных
         await mongoose.connect(config.get('mongoUri'), {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useCreateIndex: true
+            useCreateIndex: true,
+            useFindAndModify: false
         });
-
         //Прослушиваем каждое подключение
+
         await io.on('connection', socket => {
+            let socketUserData;
+            let users = [];
                 socket.on('ROOM:JOIN', data => {
+                        console.log("ROOM:JOIN");
 
-                    const roomId = data.roomId;
-                    socket.join(roomId);
-                    const userName = data.userName;
+                        const roomId = data.roomId;
+                        const userName = data.userName;
 
-                    const users = [{
-                        Name: userName,
-                        socketId: socket.id
-                    }];
+                        socketUserData = data;
 
-                    Room.updateOne({roomId: roomId}, {$push: {users}}, (err, result) => {
-                        if (err) throw err;
+                        users.push(userName);
+                        socket.join(roomId);
 
-                        console.log('данные добавлены')
-                    });
+                        console.log(users);
 
-                    Room.find({roomId}).exec((err, result) => {
-                        result.map(user => {
-                            const users = user.get('users');
-                            for (let i = 0; i < users.length; i++) {
-                                console.log(users[i].Name)
-                            }
-                        })
-                    })
+                        socket.to(roomId).emit('ROOM:JOINED', users)
+
+                        console.log('user connected', socket.id)
+                    }
+                );
 
 
-                });
-                console.log('Пользователь подключился:', socket.id)
+            socket.on('disconnecting', () => {
+                if (socketUserData) {
+                    const userDisconnected = users.find(user => user === socketUserData.userName);
+                    users.pop(userDisconnected)
+                    console.log('User socket disconnect', socketUserData, users)
+
+                }
+
+
+            })
+
             }
         );
 
         //Прослушка на порт
         server.listen(PORT, () => console.log('Server has ben started on port: ', PORT));
+
     } catch
         (e) {
         process.exit(1)
@@ -75,10 +81,47 @@ const start = async () => {
 start();
 
 
-// .exec((err, result) => {
-//     if (err) throw err;
+// Room.findOneAndUpdate(
+//     {
+//         $and: [
+//             {roomId: roomId, users: [{Name: userName}]}
+//         ]
+//     }, {
+//         $set: {
+//             users: [{
+//                 Name: userName,
+//                 socketId: socket.id
+//             }]
+//         }
+//     }, (err, result) => {
+//         if (err) throw err
+//         console.log('Данные изменены', result);
+//         return
+//     }
+// );
+
+
+// for (let i = 0; i < usersArray.length; i++) {
+//     if (userName === usersArray[i].Name) {
+//         console.log('Пользователь существует!', usersArray[i].Name);
+//         console.log('userName ', userName);
+//         console.log('roomId ', roomId);
 //
-//     return result.map((user) => {
-//         return  user.users
-//     })
-// })
+//         Room.findOne(
+//             {roomId: roomId}, (err, res) => {
+//                 if (res === null || err) throw err;
+//                 console.log('user name', res.users[i].Name);
+//             })
+//     } else {
+//         const users = [{
+//             Name: userName,
+//             socketId: socket.id
+//         }];
+//
+//         Room.updateOne({roomId: roomId}, {$push: {users}}, (err, result) => {
+//             if (err) throw err;
+//
+//             console.log('данные добавлены');
+//         });
+//     }
+// }
