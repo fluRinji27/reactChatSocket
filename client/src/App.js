84c45c94d97a8606bf4a806acc84d997c935d1cc
fluRinji <libertisiti9123@gmail.com>
2020-07-03 18:22:22 +0300

    Переработанные события: 1) JOIN Добавленные обытия: 1) SET_USERS 2) NEW_MESSAGE

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,11 @@ import JoinRoom from "./components/JoinRoom/JoinRoom";
 import Chat from "./components/Chat/Chat";
 import reducer from "./redux/reducer";
 import socket from "./hooks/socket.hook";
+import {useHttp} from "./hooks/http.hook";
 
 
 function App() {
+    const {request} = useHttp();
     const [state, dispatch] = useReducer(reducer, {
         Joined: false,
         roomId: null,
@@ -16,35 +18,49 @@ function App() {
         messages: []
 
     });
-    const onLogin = (userData) => {
+    const onLogin = async (userData) => {
         dispatch({
             type: 'JOINED',
             payload: userData
         });
 
         // Отправяем по сокета данные о попытке входа в комнате
-        socket.emit('ROOM:JOIN', userData)
+        socket.emit('ROOM:JOIN', userData);
+
+        // Получаем массив пользвателей который находятся в комнате
+        const data = await request(`/api/room/join/${userData.roomId}`, 'GET');
+
+        // Заносим пользователей в стейт
+        setUsers(data.users)
+
+
+    };
+    // Заносим пользователей в стейт
+    const setUsers = users => {
+        dispatch({
+                type: 'SET_USERS',
+                payload: users
+            }
+        );
     };
 
     useEffect(() => {
         // Фиксируем вход пользователя в комнату
-        socket.on('ROOM:JOINED', users => {
+        socket.on('ROOM:SET_USERS', setUsers);
+        socket.on('ROOM:NEW_MESSAGE', messages => {
+            console.log('messages', messages)
             dispatch({
-                    type: 'SET_USERS',
-                    payload: users
-                }
-            );
-
-            socket.emit('ROOM:JOINED', users)
-            console.log('ROOM:JOINED', users)
+                type: 'NEW_MESSAGE',
+                payload: messages
+            })
         })
     }, []);
 
-    console.log(state)
+    console.log(state.messages)
 
     return (
         <div className="App">
-            {!state.Joined ? <JoinRoom onLogin={onLogin} socket={socket}/> : <Chat users={state}/>}
+            {!state.Joined ? <JoinRoom onLogin={onLogin} socket={socket}/> : <Chat {...state}/>}
 
         </div>
     );
