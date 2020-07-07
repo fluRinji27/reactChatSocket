b914b651155feccec04ba6c9cd6770c5eb114690
fluRinji27 <50410639+fluRinji27@users.noreply.github.com>
2020-07-07 10:44:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({extended: true}));
 app.use('/api/room', require('./routes/room.routes'));
 
 if (process.env.NODE_ENV === 'production') {
-    app.use('/', express.static(__dirname, 'client', build))
+    app.use('/', express.static(__dirname, 'client', 'build'))
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirnmae, 'client', 'build', 'index.html'))
