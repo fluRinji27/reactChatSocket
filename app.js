814dc7c5c3fa175675af1faece46fa48a2cd324c
fluRinji <libertisiti9123@gmail.com>
2020-07-07 22:54:22 +0300

    edit server path

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use('/api/room', require('./routes/room.routes'));
 if (process.env.NODE_ENV === 'production') {
     app.use('/', express.static(__dirname, 'client', 'build'));
 
-    app.get('/', (req, res) => {
+    app.get('*', (req, res) => {
         console.log('Get index html');
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
