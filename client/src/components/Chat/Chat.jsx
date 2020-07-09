49a482b4be97f669e9845b3c3543735742b38baf
fluRinji27 <50410639+flurinji27@users.noreply.github.com>
2020-07-09 23:19:24 +0300

    Стилизация изменение jsx

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -4,10 +4,11 @@ import socket from "../../hooks/socket.hook";
 import './style.css'
 import Messages from "./Messages"
 
-const Chat = ({users, messages, userName, roomId, onAddMessages, allMessages}) => {
+const Chat = ({users, messages, userName, roomId, onAddMessages}) => {
 
     const [textArea, setTextArea] = useState('');
     const messageRef = useRef(null);
+    const [allMessages, setAllMessages] = useState([]);
 
     const onSendMessage = async () => {
         try {
@@ -16,10 +17,12 @@ const Chat = ({users, messages, userName, roomId, onAddMessages, allMessages}) =
                 userName,
                 text: textArea
             };
+            setAllMessages([...messages, messages])
             //Отправляем сообщение в корневой файл
             onAddMessages(message);
             // Оповещам соекеты о новом сообщении
             socket.emit('ROOM:NEW_MESSAGE', message);
+            scrollBottom(messageRef);
             // Очищаем поле ввода
             setTextArea('')
 
@@ -28,34 +31,51 @@ const Chat = ({users, messages, userName, roomId, onAddMessages, allMessages}) =
         }
 
     };
+
+    const scrollBottom = (ref) => {
+        let lastDiv;
+        console.log('Scrolling...');
+
+    }
+    console.log(messages);
+    console.log(users);
     useEffect(() => {
+
         // Фокус на последнее сообщение
-        messageRef.current.scrollTo(0, 99999)
+        messageRef.current.scrollTo = messageRef.scrollHeight
     }, [messages]);
 
 
     return (
-        <div className="Chat">
-            <div className="users">
-                <h1>Комната: {roomId}</h1>
-                <h2>В чате ({users.length}) : </h2>
-                <ul>
-                    {users.map((user, index) => <li key={user + index}><span>{user}</span></li>)}
-                </ul>
-            </div>
-            <div ref={messageRef} className="messageBox">
+        <div className="container Chat">
+            <div className="row">
+                <div className="col s12 m2 center-align users">
 
-                <Messages allMessages={allMessages} socketMessages={messages}/>
+                    <h5>Комната: {roomId}</h5>
+                    <span>В чате ({users.length}) : </span>
+                    <ul>
+                        {users.map((user, index) => <li key={user + index}>
+                            <div className="chip">
+                                <img src="#" alt="img"/>
+                                <span>{user}</span>
+                            </div>
+                        </li>)}
+                    </ul>
+                </div>
+                <div ref={messageRef} className="col s10 messageBox">
 
+                    <Messages allMessages={allMessages} socketMessages={messages}/>
 
-            </div>
-            <div className="messageInput">
+                </div>
+                <div className="col s12 m6 messageInput">
                     <textarea placeholder='Введите ваше сообщение.' value={textArea}
                               onChange={e => setTextArea(e.target.value)} cols="30" rows="5">
 
                     </textarea>
-                <button onClick={onSendMessage}>Оправить</button>
+                    <button onClick={onSendMessage}>Оправить</button>
+                </div>
             </div>
+
 
         </div>
     )
