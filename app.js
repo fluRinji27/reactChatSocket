541fd4bb33793a5dc557e338adfb070059fe0e19
fluRinji27 <50410639+flurinji27@users.noreply.github.com>
2020-07-28 15:06:10 +0300

    Оповещение других пользователей в комнате о том, что вышел текущий пользователь

--- app.js
+++ app.js
@@ -54,8 +54,8 @@ const start = async () => {
             let socketUserData;
             // Подключение пользователя к комнате
                 socket.on('ROOM:JOIN', data => {
-                    console.log('user connect', socket.id)
-                    const roomId = data.roomId
+                    console.log('user connect', socket.id);
+                    const roomId = data.roomId;
                     const userName = data.userName;
 
                         socketUserData = data;
@@ -69,7 +69,7 @@ const start = async () => {
                     // Создаем комнату в сокетах
                         socket.join(roomId);
                     // В созданную комнату вносим данные пользователя
-                    rooms.get(roomId).get('users').set(socket.id, userName)
+                    rooms.get(roomId).get('users').set(socket.id, userName);
                     // Получаем внесенные данные
                     const users = [...rooms.get(roomId).get('users').values()];
                     // Оповещаем клиент о входе пользователя
@@ -97,6 +97,19 @@ const start = async () => {
                         socket.to(socketUserData.roomId).broadcast.emit('ROOM:SET_USERS', users)
                     }
                 })
+
+            });
+            // Удаление клиента из комнаты
+            socket.on('ROOM:USER_LEFT', () => {
+                // При отключении клиента удаляем его из комнаты
+                rooms.forEach((value, roomId) => {
+                    if (value.get('users').delete(socket.id)) {
+                        const users = [...rooms.get(roomId).get('users').values()];
+                        // Повоещаем клиет о новом кол-во пользователей
+                        socket.to(socketUserData.roomId).broadcast.emit('ROOM:SET_USERS', users)
+                    }
+                })
+
             })
             }
         );
